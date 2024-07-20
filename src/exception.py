f5f9876c3ff92a900eaa8cce5af43555fe99f45f
Musashi544 <akhileshkumars020911@gmail.com>
2024-07-20 05:50:09 +0530

    ''

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-from src.logger import logging
+from src.pipeline.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     # exc_tb will store information like in which file the error occured 
@@ -20,3 +20,11 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
+    
+
+if __name__ == '__main__':
+    try: 
+        a=1/0
+    except Exception as e: 
+        logging.info('Divide By Zero') 
+        CustomException(e , sys) 
