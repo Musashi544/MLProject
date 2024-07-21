bdbd32c419757d625a4945b600d0cb87652ed983
Musashi544 <akhileshkumars020911@gmail.com>
2024-07-21 12:43:12 +0530

    Deployement Configuration

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -19,7 +19,7 @@ from src.utils import save_object
 @dataclass
 # This class creates variables for data path which is inputted to DataTransformation class.
 class DataTransformationConfig:
-    preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
+    preprocessor_obj_file_path=os.path.join('artifacts',"preprocessor.pkl")
 
 class DataTransformation:
     def __init__(self):
